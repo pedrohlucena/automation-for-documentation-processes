a5bdabc81424d275ec55b9f45d48e14d78e92037
Pedro H. Lucena <pedrohlucena413@gmail.com>
2022-11-20 11:42:04 -0300

    add transfer-docs-folder.py

---
+++ transfer-docs-folder.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+class TransferDocsFolder:
+    def __init__(self):
+        self.doc_content_repository_path = os.getenv('DOC_CONTENT_REPOSITORY_PATH')
+        self.doc_frontend_repository_path = os.getenv('DOC_FRONTEND_REPOSITORY_PATH')
+    def main(self):
+        os.system(f'rm -r {self.doc_frontend_repository_path}/docs')
+        os.system(f'cp -r {self.doc_content_repository_path}/docs {self.doc_frontend_repository_path}')
+        
+tdf = TransferDocsFolder()
+tdf.main()
