d1b1f8cf4ba92d5ecdca22dddb0a94c341256fa7
Pedro H. Lucena <pedrohlucena413@gmail.com>
2022-11-23 17:29:30 -0300

    add npm start step to transfer-docs-folder script

--- transfer-docs-folder.py
+++ transfer-docs-folder.py
@@ -10,6 +10,7 @@ class TransferDocsFolder:
     def main(self):
         os.system(f'rm -r {self.doc_frontend_repository_path}/docs')
         os.system(f'cp -r {self.doc_content_repository_path}/docs {self.doc_frontend_repository_path}')
+        os.system(f'npm start --prefix {self.doc_frontend_repository_path}')
         
 tdf = TransferDocsFolder()
 tdf.main()
