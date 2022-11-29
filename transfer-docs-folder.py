f67bebd35d40e3d4c4c36687db734c5d0ac959dd
Pedro H. Lucena <pedrohlucena413@gmail.com>
2022-11-28 21:36:28 -0300

    refactor transfer-docs-folder script

--- transfer-docs-folder.py
+++ transfer-docs-folder.py
@@ -7,10 +7,18 @@ class TransferDocsFolder:
     def __init__(self):
         self.doc_content_repository_path = os.getenv('DOC_CONTENT_REPOSITORY_PATH')
         self.doc_frontend_repository_path = os.getenv('DOC_FRONTEND_REPOSITORY_PATH')
-    def main(self):
+
+    def transferDocsToFrontendRepo(self):
         os.system(f'rm -r {self.doc_frontend_repository_path}/docs')
         os.system(f'cp -r {self.doc_content_repository_path}/docs {self.doc_frontend_repository_path}')
         os.system(f'npm start --prefix {self.doc_frontend_repository_path}')
+
+    def transferDocsToContentRepo(self):
+        os.system(f'rm -r {self.doc_content_repository_path}/docs')
+        os.system(f'cp -r {self.doc_frontend_repository_path}/docs {self.doc_content_repository_path}')
+
+    def main(self):
+        self.transferDocsToFrontendRepo()
         
 tdf = TransferDocsFolder()
 tdf.main()
