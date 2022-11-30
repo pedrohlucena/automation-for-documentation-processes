86fbff6a5b0b231046ecbd896294f1bf51924e0a
Pedro H. Lucena <pedrohlucena413@gmail.com>
2022-11-29 21:51:59 -0300

    continue in the repo after the script runs fix

--- git-utils.py
+++ git-utils.py
@@ -5,12 +5,15 @@ load_dotenv(find_dotenv())
 
 class GitUtils():
     def __init__(self):
-        self.card_number = '1111'
+        self.card_number = '2425'
         self.branch_name = f'feature/ic-{self.card_number}'
         self.docs_repo_path = os.getenv('DOC_CONTENT_REPOSITORY_PATH')
 
     def __go_to_docs_repo(self):
-        os.chdir(self.doc_repo_path)
+        os.chdir(self.docs_repo_path)
+    
+    def __stay_in_the_current_dir(self):
+        os.system("/bin/bash")
 
     def __create_local_branch(self):
         self.__go_to_docs_repo()
@@ -18,15 +21,17 @@ class GitUtils():
         os.system('git pull origin master')
         os.system(f'git checkout -b {self.branch_name}')
         # os.system(f'git merge qa --squash') # when to create branch deploy
+        self.__stay_in_the_current_dir()
 
     def __add_commit_and_push_changes(self):
         self.__go_to_docs_repo()
         os.system('git add .')
-        os.system(f'git commit -m "delivery of {self.card_number}"')
+        os.system(f'git commit -m "finish accelerators example task"')
         os.system(f'git push --set-upstream origin {self.branch_name}')
+        self.__stay_in_the_current_dir()
         
     def main(self):
         self.__create_local_branch()
 
 gu = GitUtils()
-GitUtils.main()
+GitUtils.main(gu)
