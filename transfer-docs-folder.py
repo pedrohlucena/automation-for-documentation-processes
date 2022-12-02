6d50576d3132b19374518aae3652784a25fa34ef
Pedro H. Lucena <pedrohlucena413@gmail.com>
2022-12-02 08:01:26 -0300

    update transfer-docs-folder to use other environment variable

--- transfer-docs-folder.py
+++ transfer-docs-folder.py
@@ -6,19 +6,20 @@ load_dotenv(find_dotenv())
 class TransferDocsFolder:
     def __init__(self):
         self.doc_content_repository_path = os.getenv('DOC_CONTENT_REPOSITORY_PATH')
+        self.docs_folder_in_doc_content_repository_path = os.getenv('DOCS_FOLDER_IN_DOC_CONTENT_REPOSITORY_PATH')
         self.doc_frontend_repository_path = os.getenv('DOC_FRONTEND_REPOSITORY_PATH')
 
     def transferDocsToFrontendRepo(self):
         os.system(f'rm -r {self.doc_frontend_repository_path}/docs')
-        os.system(f'cp -r {self.doc_content_repository_path}/docs {self.doc_frontend_repository_path}')
+        os.system(f'cp -r {self.docs_folder_in_doc_content_repository_path} {self.doc_frontend_repository_path}')
         os.system(f'npm start --prefix {self.doc_frontend_repository_path}')
 
     def transferDocsToContentRepo(self):
-        os.system(f'rm -r {self.doc_content_repository_path}/docs')
+        os.system(f'rm -r {self.docs_folder_in_doc_content_repository_path}')
         os.system(f'cp -r {self.doc_frontend_repository_path}/docs {self.doc_content_repository_path}')
 
     def main(self):
-        self.transferDocsToFrontendRepo()
+        self.transferDocsToContentRepo()
         
 tdf = TransferDocsFolder()
 tdf.main()
