4a6eb7279640370acb7ddb03dafc4f8175dc923c
Pedro H. Lucena <pedrohlucena413@gmail.com>
2022-12-06 20:26:44 -0300

    refactor open.pr script

--- open-pr.py
+++ open-pr.py
@@ -2,6 +2,8 @@ import os
 import webbrowser
 from library.codecommit import CodeCommit
 from library.discord import Discord
+from library.popup import Popup
+from library.jira import JiraIntegration
 from dotenv import load_dotenv, find_dotenv
 import PySimpleGUI as sg
 
@@ -9,12 +11,21 @@ load_dotenv(find_dotenv())
     
 class OpenPr:
     def __init__(self):
-        self.card_number = '2474'
-        self.pr_title = f'IC-{self.card_number}: DOC DE CLIENTE'
-        self.repository = 'repository'
-        self.origin_branch = f'feature/ic-{self.card_number}'
-        self.target_branch = 'develop'
+        self.aws_code_commit = CodeCommit(
+            self.profile, 
+            self.account_id, 
+            self.region
+        )
+        self.jira_integration = JiraIntegration()
+        self.discord_integration = Discord()
+        self.popup = Popup()
 
+        self.card = 'IC-2474'
+        self.origin_branch = f'feature/{self.card.lower()}'
+        self.target_branch = 'develop'
+        self.repository = os.getenv('DOC_CONTENT_REPOSITORY_NAME')
+
+        self.pr_title = f'{self.card_number}: {self.jira_integration.get_card_name(self.card)}'
         self.merge_type = 'THREE_WAY_MERGE'
         self.region = os.getenv('REGION')
         self.profile = os.getenv('PROFILE')
@@ -22,14 +33,6 @@ class OpenPr:
         self.pr_channel_id = os.getenv('PR_CHANNEL_ID')
         self.approval_groups_ids = [os.getenv('FRONTEND_GROUP_ID'), os.getenv('ARCHITECTURE_GROUP_ID')]
         
-        self.aws_code_commit = CodeCommit(
-            self.profile, 
-            self.account_id, 
-            self.region
-        )
-        
-        self.discord_integration = Discord()
-
     def main(self):
         if self.target_branch == 'master':
             self.__create_delivery_branch()
@@ -99,28 +102,8 @@ class OpenPr:
 
         self.origin_branch = delivery_branch_name
 
-    def __mount_conflict_popup(self):
-        column_to_be_centered = [
-            [sg.Text(
-                'Já resolveu os conflitos? Posso terminar de fazer a subida?', 
-                size=(50, 1), 
-                justification="center")
-            ], 
-            [sg.Button("Sim"), sg.Button("Não")]
-        ]
-
-        layout = [
-            [sg.VPush()],
-            [sg.Push(), sg.Column(column_to_be_centered,element_justification='c'), sg.Push()],
-            [sg.VPush()]
-        ]
-
-        return layout
-
     def __render_conflict_popup(self):
-        layout = self.__mount_conflict_popup()
-
-        window = sg.Window("Conflitos na subida...", layout)
+        window = self.popup.__mount_conflict_popup()
 
         while True:
             event, values = window.read()
@@ -128,36 +111,13 @@ class OpenPr:
                 self.__open_pr()
                 break
 
-    def __mount_send_discord_message_popup(self, pr_url):
-        column_to_be_centered = [
-            [sg.Text('Link da PR:', size=(45, 1), justification="center")],
-            [sg.Text(
-                pr_url, 
-                tooltip=pr_url, 
-                enable_events=True, 
-                size=(140, 1), 
-                justification="center",
-                key=f'URL {pr_url}')],
-            [sg.Text('A pr já foi revisada? Posso mandar a mensagem no discord pro pessoal?', size=(60, 1), justification="center")],
-            [sg.Button("Sim"), sg.Button("Não")]
-        ]
-
-        layout = [
-            [sg.VPush()],
-            [sg.Push(), sg.Column(column_to_be_centered,element_justification='c'), sg.Push()],
-            [sg.VPush()]
-        ]
-
-        return layout
-
     def __get_pr_url(self, pr_id):
         return f'https://{self.region}.console.aws.amazon.com/codesuite/codecommit/repositories/{self.repository}/pull-requests/{pr_id}/details?region={self.region}'
 
     def __render_send_discord_message_popup(self, pr_id):
         pr_url = self.__get_pr_url(pr_id)
 
-        layout = self.__mount_send_discord_message_popup(pr_url)
-        window = sg.Window("Pó manda?", layout)
+        window = self.popup.__mount_send_discord_message_popup(pr_url)
 
         while True:
             event, values = window.read()
