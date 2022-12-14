9a21f2ef44c94319d319eac55a8dd7d64743b2c7
Pedro H. Lucena <pedrohlucena413@gmail.com>
2022-12-13 22:25:25 -0300

    refactor git_utils again

--- git_utils.py
+++ git_utils.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 from library.os import Os
-from doc_state import DocState
+from library.doc_state import DocState
 from library.git import Git
 
 load_dotenv(find_dotenv()) 
@@ -9,20 +9,21 @@ load_dotenv(find_dotenv())
 class GitUtils():
     def __init__(self):
         self.doc_state = DocState()
-        self.docs_repo_path = os.getenv('DOC_CONTENT_REPOSITORY_PATH')
-        
         self.os = Os()
         self.git = Git()
-    
+
+        self.branch_name = self.doc_state.origin_branch_name
+        self.docs_repo_path = os.getenv('DOC_CONTENT_REPOSITORY_PATH')
+        
     def __create_local_branch(self):
         self.os.go_to_docs_repo()
-        self.git.create_local_branch(self.doc_state.origin_branch_name)
+        self.git.create_local_branch(self.branch_name)
         self.os.stay_in_the_current_dir()
 
     def __add_commit_and_push_changes(self):
         self.os.go_to_docs_repo()
         self.git.add_and_commit('commit message')
-        self.git.push_branch_to_codecommit(self.doc_state.origin_branch_name)
+        self.git.push_branch_to_codecommit(self.branch_name)
         self.os.stay_in_the_current_dir()
 
     def main(self):
