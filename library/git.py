4984abc5a99dc118fa6557166d23f81885dee200
Pedro H. Lucena <pedrohlucena413@gmail.com>
2022-12-15 18:49:09 -0300

    implement create_local_branch_controller on git library

--- library/git.py
+++ library/git.py
@@ -1,5 +1,7 @@
 import os
-from dotenv import load_dotenv, find_dotenv
+from subprocess import subprocess, CalledProcessError
+from dotenv import load_dotenv, find_dotenv  
+from library.popup import Popup
 
 load_dotenv(find_dotenv()) 
 
@@ -15,4 +17,38 @@ class Git():
         os.system('git checkout master')
         os.system('git pull origin master')
         os.system(f'git checkout -b {local_branch_name}')
-        
+
+    def __verify_if_local_branch_exists(branch): 
+        try:
+            subprocess.Popen(['git', 'show-ref', '--verify', f'refs/heads/{branch}'], stderr=subprocess.STDOUT)
+            return True
+        except CalledProcessError as e:
+            if 'fatal not a valid ref' in str(e.output):
+                return False
+
+    def __delete_local_branch(branch):
+        os.system(f'git branch -D {branch}')
+
+    def __create_local_branch(branch):
+        os.system(f'git checkout -b {branch}')
+
+    def create_local_branch_controller(self, repo, branch):
+        popup = Popup()
+
+        local_branch_exists = self.__verify_if_local_branch_exists(branch)
+
+        if local_branch_exists:
+            window = popup.mount_branch_already_exists_popup(repo, branch)
+
+            while True:
+                event, values = window.read()
+                if event == "Seguir o processo com a branch já existente":
+                    break
+                elif event == "Recriar a branch e seguir com o processo":
+                    self.__delete_local_branch(branch)
+                    self.__create_local_branch(branch)
+                    break
+                elif event == "Encerrar o processo":
+                    exit()
+        else:
+            self.__create_local_branch(branch)
