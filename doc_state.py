af583fbdbdb283ae2bcd13080f9212763e308a28
Pedro H. Lucena <pedrohlucena413@gmail.com>
2022-12-22 20:48:47 -0300

    move card state to root dir

---
+++ doc_state.py
@@ -0,0 +1,4 @@
+class DocState:
+    def __init__(self):
+        self.card_key = 'IC-7625'
+        self.origin_branch_name = 'qa' # f'feature/{self.card_key}'.replace('IC', 'ic')
--- library/doc_state.py
+++
@@ -1,4 +0,0 @@
-class DocState:
-    def __init__(self):
-        self.card_key = 'IC-7383'
-        self.origin_branch_name = f'feature/{self.card_key}'.replace('IC', 'ic')
